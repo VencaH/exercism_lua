e7137c648045c0db757402ca543db31c31a4fe18
Venca H <venca.hyncica@gmail.com>
2024-11-02 23:00:38 +0100

    init commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Basic python flake";
+
+  inputs  = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    with pkgs;
+    {
+      devShells.default = mkShell {
+        buildInputs = [
+          lua
+        ];
+        packages = [
+          (lua5_2.withPackages ( lua-pkgs: [
+            lua-pkgs.busted  
+          ]))
+        ];
+      };
+    }
+  );
+}
